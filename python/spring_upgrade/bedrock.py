0f588e780ed587295d931c16600768c9a518a648
Leandra Nealer <lrosenealer@gmail.com>
2026-02-03 14:25:16 -0600

    wip

--- python/spring_upgrade/bedrock.py
+++ python/spring_upgrade/bedrock.py
@@ -2,6 +2,8 @@ import boto3
 from botocore.client import Config
 from langchain_community.llms import Bedrock
 from langchain_core.prompts import PromptTemplate
+import re
+import json
 
 from utils import get_logger
 
@@ -32,6 +34,26 @@ Provide a response in JSON format with the following keys:
 </code>
 """,
 )
+
+class Model:
+    """Model class for GenAI."""
+
+    def upgrade_code(self, version, source_code_map):
+        """Trigger the code fix generation process."""
+        prompt = self._create_prompt(stack_trace, source_code_map)
+        content = self._invoke(prompt)
+        cleaned_content = self.clean_result(content)
+        return json.loads(cleaned_content)
+
+    def clean_result(self, content):
+        """Clean the response from the model."""
+        cleaned_result = content.replace("```", "")
+        cleaned_result = remove_newlines(cleaned_result)
+        cleaned_result = cleaned_result.strip()
+        cleaned_result = cleaned_result.rstrip(",")
+        # Replace \n with \\n to escape newlines in JSON
+        cleaned_result = cleaned_result.replace("\n", "\\n")
+        return cleaned_result
 
 
 class Claude(Model):
@@ -88,24 +110,3 @@ def remove_newlines(json_string):
     result = re.sub('(?<!")\\n', "", result)
     result = re.sub("\\n(?= *})", "", result)
     return result
-
-
-class Model:
-    """Model class for GenAI."""
-
-    def upgrade_code(self, version, source_code_map):
-        """Trigger the code fix generation process."""
-        prompt = self._create_prompt(stack_trace, source_code_map)
-        content = self._invoke(prompt)
-        cleaned_content = self.clean_result(content)
-        return json.loads(cleaned_content)
-
-    def clean_result(self, content):
-        """Clean the response from the model."""
-        cleaned_result = content.replace("```", "")
-        cleaned_result = remove_newlines(cleaned_result)
-        cleaned_result = cleaned_result.strip()
-        cleaned_result = cleaned_result.rstrip(",")
-        # Replace \n with \\n to escape newlines in JSON
-        cleaned_result = cleaned_result.replace("\n", "\\n")
-        return cleaned_result
