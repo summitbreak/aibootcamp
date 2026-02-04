640a78f6cc40981122a02d1202f0420ec1f99594
Leandra Nealer <lrosenealer@gmail.com>
2026-02-04 09:30:49 -0600

    wip

--- python/spring_upgrade/lambda_handler.py
+++ python/spring_upgrade/lambda_handler.py
@@ -3,21 +3,19 @@ import tempfile
 import time
 
 from git_utils import GitHubProvider, clone_repo, create_branch, update_source_code
-from utils import get_config, get_logger
+from utils import get_logger, get_config
 from bedrock import Claude
 
 logger = get_logger()
 
 PARAMETER_NAMES = (
-    "model_provider",
-    "repo_url",
-    "repo_name",
-    "repo_api_url",
-    "repo_api_key",
-    "repo_ssh_private_key",
-    "cloudwatch_log_group_name",
+    "ssh_private_key"
 )
-PARAMETER_STORE_PREFIX = os.environ["PARAMETER_STORE_PREFIX"]
+MODEL_AWS_REGION = "us-east-1"
+
+SSH_PRIVATE_KEY_FILENAME = "ssh_private_key"
+
+PARAMETER_STORE_PREFIX = "spring_upgrade_lambda_"# os.environ["PARAMETER_STORE_PREFIX"]
 MODEL_AWS_REGION = "us-east-1"
 
 SSH_PRIVATE_KEY_FILENAME = "ssh_private_key"
@@ -31,20 +29,20 @@ def lambda_handler(event, context):
     request = event["body"]
     spring_version = request["spring_version"]
     repo_url = request["github_url"]
-    # api_key = request["repo_api_key"]
     repo_name = repo_url.split("/")[-1]
 
     logger.info(f"Retrieving config")
     config = get_config(PARAMETER_STORE_PREFIX, PARAMETER_NAMES)
-    api_key = config["api_key"]
+    ssh_private_key = config["ssh_private_key"]
+    api_key = "foo"
 
     # Select a model provider to perform the code generation
     provider = Claude(model_aws_region=MODEL_AWS_REGION)
 
     # Prepare SSH credentials for cloning the target repo
     tmpdir = tempfile.mkdtemp()
-    ssh_private_key = os.path.join(tmpdir, "ssh_private_key")
-    write_ssh_key(config["repo_ssh_private_key"], ssh_private_key)
+    ssh_private_key_path = os.path.join(tmpdir, "ssh_private_key")
+    write_ssh_key(ssh_private_key, ssh_private_key_path)
 
     # Clone the target repo
     git_provider = GitHubProvider(api_key, repo_url)
@@ -61,7 +59,7 @@ def lambda_handler(event, context):
     update_source_code(result["source_code"], target_repo_dir)
 
     # Create a branch and commit/push the code to the source repo
-    branch_name = f"fix-code-{round(time.time())}"
+    branch_name = f"upgrade-code-{round(time.time())}"
     branch_created = create_branch(branch_name, repo, result["description"])
     if not branch_created:
         logger.info("No changes were made, exiting.")
@@ -133,11 +131,9 @@ if __name__ == "__main__":
 
     class MockContext:
         aws_request_id = "1234"
-
-    error = """
-         [ERROR] KeyError: \'order_items\'\nTraceback (most recent call last):\n  File "/var/task/handlers/create_order.py", line 14, in handler\n    order_items = body["order_items"]
-         """
-    handler(
-        {"Records": [{"body": error}]},
+    lambda_handler({
+        "github_url": "https://github.com/summitbreak/aibootcamp",
+        "spring_version": "Spring boot 2.7"
+        },
         MockContext(),
     )
